7ee30254ec78de209327c98787aaa8031d4c2105
AgentAntelope <alex.sunderland@gmail.com>
2012-09-07 16:13:05 +0100

    Added 404 page for ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user_session, :current_user
   protected
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_missing
+  def render_missing
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+      format.xml  { head :not_found }
+    end
+end
   def current_user_session
     @current_user_session ||= UserSession.find
   end
