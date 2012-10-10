5445e7296df9d8ae468a5002b8e15717fa354dc6
AgentAntelope <alex.sunderland@gmail.com>
2012-10-10 10:52:33 +0100

    Added more tests for the posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,4 +7,28 @@ describe PostsController do
       response.should be_success
     end
   end
+  describe "create" do
+    before do
+      @user = FactoryGirl.create(:user)
+      UserSession.create!(:login => @user.login, :password => @user.password)
+    end
+    describe "with good params" do
+      it "saves the post" do
+        lambda {
+          post :create, :post => { :title => "Test title", :content => "Test content"}
+        }.should change {Post.count}
+      end
+      
+      it "redirects" do
+        post :create, :post => { :title => "Test title", :content => "Test content"}
+        response.should be_redirect
+      end
+      
+      it "adds the post to the current user" do
+        lambda {
+          post :create, :post => { :title => "Test title", :content => "Test content"}
+        }.should change {@user.posts.count}
+      end
+    end
+  end
 end
